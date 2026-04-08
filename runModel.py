9f7c61ff24e78542f0d8104d805434c5807a5177
Train050 <keegan.sepiol@ufl.edu>
2026-04-08 00:08:45 -0400

    Changed pandas csv for csv, and trying to read code snippets from the data file

--- runModel.py
+++ runModel.py
@@ -2,16 +2,23 @@ from ollama import generate
 from ollama import chat
 import time
 import pandas as pd
+import csv
 
 llmModels = ['gemma4:e2b', 'deepseek-r1:1.5b', 'llama3.2:3b']
+llmPhrase = 'You are an senior level programmer. '
 
 #Helpful resource I followed for my approach: https://medium.com/@jonigl/using-ollama-with-python-a-simple-guide-0752369e1e55
 #Time resource used: https://docs.python.org/3/library/time.html
 
-codeSnippets = pd.read_csv("./MSR_data_cleaned./MSR_data_cleaned.csv")
-
-startTime = time.time()
-#response = chat(model='gemma4:e2b', messages=[{'role': 'user', 'content': 'Hello'}],)
-stopTime = time.time()
-
-#print(response.message.content)
+#https://docs.python.org/3/library/csv.html
+with open('./MSR_data_cleaned./MSR_data_cleaned.csv', newline='') as csvfile:
+    codeSnippets = csv.DictReader(csvfile, delimiter=',')
+    for row in codeSnippets:
+        print(row['func_before'])
+        print(row['func_after'])
+        
+    startTime = time.time()
+    #response = chat(model='gemma4:e2b', messages=[{'role': 'user', 'content': 'Hello'}],)
+    stopTime = time.time()
+    #print(response.message.content)
+    
