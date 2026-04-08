966c3efe42cc0c58dd1bb916c4efbc671abaf26f
Train050 <keegan.sepiol@ufl.edu>
2026-04-07 21:35:20 -0400

    Also downloaded the cleaned data from research GitHub Repo
    Noted models I'm using so I don't forget when running Ollama

--- runModel.py
+++ runModel.py
@@ -2,6 +2,8 @@ from ollama import generate
 from ollama import chat
 import time
 
+llmModels = ['gemma4:e2b', 'deepseek-r1:1.5b', 'gemma4:e2b']
+
 #Helpful resource I followed for my approach: https://medium.com/@jonigl/using-ollama-with-python-a-simple-guide-0752369e1e55
 #Time resource used: https://docs.python.org/3/library/time.html
 startTime = time.time()
